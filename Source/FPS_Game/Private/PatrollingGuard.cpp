56f60e2aa972d645de2e0d9544435932241063d9
Denil George <denil.george@gmail.com>
2016-11-16 15:41:33 -0800

    Lecture 214: Before deleting C++ class

---
+++ Source/FPS_Game/Private/PatrollingGuard.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPS_Game.h"
+#include "PatrollingGuard.h"
+
+
+
+
---
+++ Source/FPS_Game/Public/ChooseNextWaypoint.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_GAME_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
